7ce000e6786ac6463bf81be96475e5000f854ffb
Paul Barber <paul_j_barber@yahoo.co.uk>
2025-10-15 11:49:54 +0000

    Load the 'towns' data from DfC

--- process.py
+++ process.py
@@ -1,5 +1,26 @@
 # %%
 import pandas as pd
+import os
+import requests
+import json
+
+def download_file_if_not_exists(url, fname=None, jsonkey=None):
+    if fname is None:
+        fname = os.path.basename(url)
+    if not os.path.isfile(fname):
+        session = requests.Session()
+        if jsonkey is None:
+            with session.get(url, stream=True) as stream:
+                stream.raise_for_status()
+                with open(fname, 'wb') as f:
+                    for chunk in stream.iter_content(chunk_size=8192):
+                        f.write(chunk)
+        else:
+            resp = session.get(url)
+            resp.raise_for_status()
+            print(resp.json())
+            with open(fname, 'w') as f:
+                json.dump(resp.json().get(jsonkey), f)
 
 # %%
 dom = pd.read_csv('data/domestic-property-vacancy-rates-by-district-council.csv')
@@ -55,5 +76,29 @@ full.pivot(
     columns='District Council', 
     values='Non-Domestic Vacant Properties Ratio'
 ).to_csv('data/property-vacancy-rates-by-district-council-non-domestic-vacant-ratio.csv', index=True)
+
+# %%
+download_file_if_not_exists('https://www.communities-ni.gov.uk/sites/default/files/2025-08/tcd-non-domestic-property-nav-planning-applications-vacancy-rates-floor-space.xlsx', 'data/tcd-non-domestic-property-nav-planning-applications-vacancy-rates-floor-space.xlsx')
+# Convert the Excel to a dataframe with multi-index columns
+tcd = pd.read_excel('data/tcd-non-domestic-property-nav-planning-applications-vacancy-rates-floor-space.xlsx', sheet_name='Non-Dom Vacancy Rates ', header=list(range(5,8)))
+tcd.columns = tcd.columns.droplevel(1)
+# Discard all-NA columns
+tcd = tcd.dropna(axis=1, how='all')
+# Fix incorrect year in column multiindex
+tcd.columns = tcd.columns.set_levels([x.replace('20173', '2017') for x in tcd.columns.levels[0]], level=0)
+# Clean up inferred column name
+tcd.columns = tcd.columns.set_levels([x.replace('Unnamed: 1_level_0', '') for x in tcd.columns.levels[0]], level=0)
+# Collapse multi-index columns into single level with format "firstlevel - secondlevel"
+tcd.columns = [f"{col[1]} - {col[0]}" if col[0] != '' else col[1] for col in tcd.columns]
+# Adjust to long format
+tcd_long = pd.wide_to_long(tcd, stubnames=['No. of Properties', 'No. of Vacant Properties', '% of Vacant Properties'], i=['TOWN CENTRE'], j='Date', sep=' - ', suffix='.*').reset_index()
+# Use a proper date format
+tcd_long['Date'] = pd.to_datetime(tcd_long['Date'], format='%d %B %Y')
+
+# %%
+tcd_long.to_csv('data/tcd-non-domestic-property-vacancy-rates.csv')
+
+# %%
+pd.melt(tcd_long, id_vars=['TOWN CENTRE', 'Date'], var_name='Metric', value_name='Value').pivot(index=['Date', 'Metric'], columns='TOWN CENTRE', values='Value')
 
 # %%
